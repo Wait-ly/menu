653f92391c728467fcc0efc348d3d365920f35d5
Wesley Luk <wesleyluk@wesleys-mbp.gateway.sonic.net>
2019-08-31 00:26:18 -0700

    Added hoverable feature to visibility button

--- client/HideButton.jsx
+++ client/HideButton.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import styles from './css_modules/hideButton.css';
 
-const HideButton = (props) => {
-  const { visibility, handleVisibility } = props;
-  return (
-    <button type="button" className={styles.btn} onClick={() => handleVisibility()}>{visibility}</button>
-  );
-};
+class HideButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hovered: false,
+    };
+    this.handleOnHover = this.handleOnHover.bind(this);
+  }
+
+  handleOnHover() {
+    const { hovered } = this.state;
+    this.setState({ hovered: !hovered });
+  }
+
+  render() {
+    const { visibility, handleVisibility } = this.props;
+    const { hovered } = this.state;
+    return (
+      <button type="button" onMouseEnter={this.handleOnHover} onMouseLeave={this.handleOnHover} className={hovered ? styles.hovered : styles.unhovered} onClick={() => handleVisibility()}>{visibility}</button>
+    );
+  }
+}
 
 export default HideButton;
